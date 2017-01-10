a8b4652571405672b3bd6a65764955d0a29ce7c7
yaro97 <wyzh97@gmail.com>
2017-01-10 16:47:33 +0800

    “updateblogsource”

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@ hexo g
 git add .
 git commit -m “updateblogsource”
 git push origin master
-cp -R d:/tools/linux/hexo/blog/public/. d:/tools/linux/hexo/yaro97.github.io/
-cd d:/tools/linux/hexo/yaro97.github.io/
+cp -R D:/tools/babun/.babun/cygwin/home/yaro/blog/public. D:/tools/babun/.babun/cygwin/home/yaro/yaro97.github.io/
+cd D:/tools/babun/.babun/cygwin/home/yaro/yaro97.github.io/
 git add .
 git commit -m “updateblog”
 git push origin master
